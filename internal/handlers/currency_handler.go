77fbae1fb36c2c35c502e2e0519784a73320b586
steve rigby <steve.rigby@rewardstyle.com>
2024-06-28 10:57:53 -0400

    running - no tests yet

--- internal/handlers/currency_handler.go
+++ internal/handlers/currency_handler.go
@@ -20,8 +20,9 @@ func ConvertHoldings(usdAmount float64, crypto1, crypto2 string) (string, error)
 		return "", fmt.Errorf("invalid cryptocurrency provided")
 	}
 
-	amount1 := usdAmount * 0.70 / rate1
-	amount2 := usdAmount * 0.30 / rate2
+	amount1 := usdAmount * 0.70 * rate1
+	amount2 := usdAmount * 0.30 * rate2
 
+	// return fmt.Sprintf("amount1 %f, amount2 %f", amount1, amount2), nil
 	return fmt.Sprintf("$%.2f => %.4f %s $%.2f => %.4f %s", usdAmount*0.70, amount1, crypto1, usdAmount*0.30, amount2, crypto2), nil
 }
